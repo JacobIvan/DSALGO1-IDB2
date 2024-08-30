fb8fbfc09590a26c11a33b95d855b695f01b2da6
JacobIvan <20233564@s.ubaguio.edu>
2024-08-30 16:24:04 +0800

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Excercise 1
 userinput = float (input ("How much money ya got': " ) ) ;
 nintendo = 100
 affordabol = int(userinput/100)
@@ -8,25 +9,25 @@ print ("You needa hustle cuh you need \u20b1",needmoney," if ya want more  ")
 
 if userinput < 500:
     print("You cant be using mah code broke ahh")
+    # Exercise 1
     for x in range(1, 5):
         print ( "get a job")
 else:
-    for x in range (99,0,-1):
-        print(x, "Bottles of beer")
+
+    #Exercise 1.2
     print ("\nFactoring numbers")
     inputfactorial = int (input("Enter a number foo: "))
     factorial = 1
     for i in range(1, inputfactorial + 1):
         factorial *= i
     print ("Factorial of your number is ", factorial)
-    pass;
-factorcount = 0
+
+
+
+#Exercise 1.3
 factoring= int(input("input a number to count the factors: "))
 for x in range (1,factoring+1):
     if factoring % x==0:
-        factorcount + 1
         print(x ,end = ' ')
-
-print("Factor count", factorcount)
 
 
